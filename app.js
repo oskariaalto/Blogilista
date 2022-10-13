f197a2fef56acdf9ebeee81813093a6b8286c8af
oskariaalto <114106396+oskariaalto@users.noreply.github.com>
2022-10-13 15:22:44 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('express-async-errors')
+
+const {info , error} = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() =>{
+        info('connected to MongoDB')
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.tokenExtractor)
+app.use(middleware.userExtractor)
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.errorHandler)
+
+
+module.exports = app
